87e222606d76fe78359ba8fde1b78a90ceef414f
Piyush Garg <piyushgarg.dev@gmail.com>
2026-01-22 22:56:59 +0530

    Feat/multiply (#19)
    * Add multiply function to index.js for basic arithmetic operations
    
    * Add multiply function call to index.js for arithmetic operations
    
    * Add another multiply function call to index.js for extended arithmetic operations
    
    * Add another multiply function call to index.js for further arithmetic operations
    
    * Add another multiply function call to index.js for enhanced arithmetic operations
    
    * Implement error handling in multiply function to disallow negative inputs

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ function sub(a, b) {
     return a - b
 }
 
+function multiply(a, b) {
+    if (a < 0 || b < 0) throw new Error('No neg allowed')
+    return a * b
+}
+
 add(2, 5)
 add(10, 23)
 add(40, 42)
 
 
 sub(10, 3)
+
+
+multiply(2, 3)
+multiply(20, 35)
+multiply(20, 11)
+multiply(20, 110)
